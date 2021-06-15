b5335f4e3ae84a196a59802213dcfa770120690b
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-15 16:32:27 +0200

    fait + correction

---
+++ Exercice7.1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 7.1</title>
+</head>
+<body>
+    <h1>Exercice 1 Partie 7 : Les Formulaires PHP</h1>
+    <form action="user.php" method="GET">
+        <input type="text" name="lastname" placeholder="Nom">
+        <input type="text" name="firstname" placeholder="Prénom">
+        <input type="submit" name="submit" value="Valider le formulaire">
+    </form>
+</body>
+</html>
